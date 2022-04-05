1d2cac30ef3439e275a4e33598a43c0e4952ce7d
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-04-05 19:26:30 -0400

    dummy data rendered

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, { useState } from 'react';
 import './App.css';
 
 
+
+const memberList = [
+  {
+    name: 'Mason Lyons',
+    email: 'masonLyons@gmail.com',
+    role: 'backend engineer'
+  },
+  {
+    name: 'Shannon DeLap ',
+    email: 'delaptrap@gmail.com',
+    role: 'designer'
+  },
+  {
+    name: 'Peter Farris',
+    email: 'farrisP@gmail.com',
+    role: 'intern'
+  }
+]
+
+
 const initialFormValues = {
   name: '',
   email: '',
@@ -10,13 +30,25 @@ const initialFormValues = {
 
 function App() {
 
-  const [members, setMembers] = useState([]); //holds the form data
+  const [members, setMembers] = useState(memberList); //holds the form data
   const [formValues, setFormValues] = useState(initialFormValues); //holds the form values
-  console.log(members, formValues);
+  //console.log(members, formValues);
 
   return (
     <div className="App">
       <h1>Team Builder</h1>
+
+      {
+        memberList.map((member, idx) => {
+          return (
+            <div key={idx}>
+              <h2>{member.name}</h2>
+              <p>{member.email}</p>
+              <p>{member.role}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
