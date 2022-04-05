9e31987ab089dff911e5c3b80aa34ac5daaac59f
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-04-05 18:51:38 -0400

    set up state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import React, { useState } from 'react';
 import './App.css';
 
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: ''
+}
+
 function App() {
+
+  const [members, setMembers] = useState([]); //holds the form data
+  const [formValues, setFormValues] = useState(initialFormValues); //holds the form values
+  console.log(members, formValues);
+
   return (
     <div className="App">
       <h1>Team Builder</h1>
