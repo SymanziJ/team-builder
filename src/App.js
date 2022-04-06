af97b61bae970c3c88ad61e669acecee3eddab96
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-04-05 20:43:50 -0400

    form complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Members from './components/Members';
+import Form from './components/Form';
 
 
 const memberList = [
@@ -28,18 +29,40 @@ const initialFormValues = {
   role: ''
 }
 
+
 function App() {
 
   const [members, setMembers] = useState(memberList); //holds the form data
   const [formValues, setFormValues] = useState(initialFormValues); //holds the form values
   //console.log(members, formValues);
 
+  const updateForm = (inputName, inputValue) => {
+    setFormValues( { ...formValues, [inputName]: inputValue});
+  }
+
+  const submitForm = () => {
+    const newMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role.trim()
+    };
+    
+    setMembers([newMember, ...members]);
+    setFormValues(initialFormValues);
+  }
+
   return (
     <div className="App">
       <h1>Team Builder</h1>
 
+      <Form 
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+      />
+
       {
-        memberList.map((member, idx) => {
+        members.map((member, idx) => {
           return (
             <Members key={idx} details={member}/>
           )
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,1 +1,50 @@
 import React from 'react';
+
+function Form(props) {
+    const {values, update, submit} = props;
+
+    const OnChange = evt => {
+        const { name, value } = evt.target;
+        update(name, value);
+    }
+
+    const onSubmit = evt => {
+        evt.preventDefault();
+        submit();
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <label>Name: 
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="first and last name"
+                    value={values.name}
+                    onChange={OnChange}
+                />
+            </label>
+            <label>Email: 
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    value={values.email}
+                    onChange={OnChange}
+                />
+            </label>
+            <label>Role: 
+                <input
+                    type="text"
+                    name="role"
+                    placeholder="role"
+                    value={values.role}
+                    onChange={OnChange}
+                />
+            </label>
+            <button>Add new member!</button>
+        </form>
+    )
+}
+
+export default Form;
